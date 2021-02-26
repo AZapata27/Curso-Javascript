32b59394c85a1b728e88713c24afcc2149d1fd3a
Andres Zapata <azapata@uc.edu.co>
2021-02-25 23:17:32 -0500

    Strings y metodos

--- clase-1/scripts.js
+++ clase-1/scripts.js
@@ -1,5 +1,53 @@
-const numero = 4.5;
+let numero = 4.5;
 
-const palabra = "Hola mundo";
+let palabra = "Hola mundo";
 
-console.log(numero + palabra);
+console.log(numero + palabra + palabra);
+
+
+
+
+// operadores matematicos
+
+let num1 = 2;
+
+let num2= 5;
+
+
+let result= num1 + num2 ;
+
+console.log(result);
+
+result= num1 - num2 ;
+
+console.log(result);
+
+result= num1 * num2 ;
+
+console.log(result);
+
+
+result= num1 / num2 ;
+
+console.log(result);
+
+result= num1 % num2 ;
+
+console.log(result);
+
+
+
+//Operadores de Asigancion 
+
+num1 += 3;
+console.log( num1);
+
+num1 -= 3;
+console.log(num1);
+
+//Operadores de incremente y decremento
+
+num1++;
+
+num1++;
+console.log(num1++);
---
+++ clase-2/scripts.js
@@ -0,0 +1,29 @@
+let cadena = "Hola mundo";
+
+console.log(cadena.length);
+
+console.log(cadena.toUpperCase());
+
+console.log(cadena.toLowerCase());
+
+console.log(cadena.indexOf("o"));
+
+console.log(cadena.replace("Hola", "hellow"));
+
+
+let nombre = 'juan';
+
+let apellido = 'gomez';
+
+let edad = 20;
+
+console.log(`Hola ${nombre} ${apellido}. Tienes ${edad} años `);
+
+
+let prueba= 'hola mundo probando split';
+
+let res =  prueba.split(' ');
+
+console.log(res);
+
+
