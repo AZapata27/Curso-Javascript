10ec9780e706f876ddc33372efa9557ca0b29d28
Andres Zapata <azapata@uc.edu.co>
2021-02-26 20:51:34 -0500

    modificacion de carpetas

---
+++ clase-1/scripts.js
@@ -0,0 +1,53 @@
+let numero = 4.5;
+
+let palabra = "Hola mundo";
+
+console.log(numero + palabra + palabra);
+
+
+
+
+// operadores matematicos
+
+let num1 = 2;
+
+let num2= 5;
+
+
+let result= num1 + num2 ;
+
+console.log(result);
+
+result= num1 - num2 ;
+
+console.log(result);
+
+result= num1 * num2 ;
+
+console.log(result);
+
+
+result= num1 / num2 ;
+
+console.log(result);
+
+result= num1 % num2 ;
+
+console.log(result);
+
+
+
+//Operadores de Asigancion 
+
+num1 += 3;
+console.log( num1);
+
+num1 -= 3;
+console.log(num1);
+
+//Operadores de incremente y decremento
+
+num1++;
+
+num1++;
+console.log(num1++);
