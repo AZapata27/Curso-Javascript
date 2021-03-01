a6253f8d2df70083f8fed945c5bf71aaa3426544
Andres Zapata <azapata@uc.edu.co>
2021-02-28 22:02:14 -0500

    Bucles en JS (do, do while, for -for in- for of)

---
+++ clase-6/scripts.js
@@ -0,0 +1,33 @@
+// bucles
+
+let pass = [];
+
+while (pass != "hola") {
+  pass = prompt("introduzca su contraseña");
+}
+console.log("fin del bucle");
+
+
+
+
+
+
+
+do{
+   pass=prompt('introduzca contraseña');
+}while(pass!= 'hola');
+
+
+
+// bucle for of y for in
+
+
+
+
+1
+
+
+
+
+
+
