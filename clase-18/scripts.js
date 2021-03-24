1770d7903ed70c1316a4db0239b7cb67ae69ff85
Andres Zapata <azapata@uc.edu.co>
2021-03-23 19:23:47 -0500

    AXIOS metodo GET

--- clase-18/scripts.js
+++ clase-18/scripts.js
@@ -38,3 +38,98 @@ async function funcionAsyncronadDeclarada (value){
 
 
 }
+
+
+/* 
+
+
+
+
+
+
+button.addEventListener('click',()=>{
+
+
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res=>res.ok ? Promise.resolve(res): Promise.reject(res))
+    .then(res=>res.json())
+    .then(res=>{
+
+        const list = document.getElementById('list');
+
+        const fragment = document.createDocumentFragment();
+
+        for (const userInfo of res) {
+
+            const listItem=  document.createElement('LI');
+            listItem.textContent=  `${userInfo.id} - ${userInfo.name}`;
+
+            fragment.appendChild(listItem);
+            
+        }
+
+        
+        list.appendChild(fragment);
+
+
+
+    });
+    const newPost={
+        title: 'a new post',
+     body: 'lorem ipsum asdgasdgasgagafhfhdgggggggggggggggggggfhdfghdgfh'
+     , userId: 1
+    }
+    fetch('https://jsonplaceholder.typicode.com/users',{
+            method: 'POST',
+            body : JSON.stringify(newPost),
+            headers:{
+                        'Content-type': 'application/json'
+            }
+
+    }).then(res=>res.json())
+    .then(data=>console.log(data));
+    fetch('https://jsonplaceholder.typicode.com/users',{
+            method: 'POST',
+            body : JSON.stringify(newPost),
+            headers:{
+                        'Content-type': 'application/json'
+            }
+
+    }).then(res=>res.json())
+    .then(data=>console.log(data));
+});
+ */
+const button = document.getElementById('button');
+
+button.addEventListener('click',()=>{
+            axios({
+                method: 'GET',
+                url: 'https://jsonplaceholder.typicode.com/users'
+
+
+
+            }).then(res=>{
+                
+                
+                const list = document.getElementById('list');
+
+                const fragment = document.createDocumentFragment();
+        
+                for (const userInfo of res.data) {
+        
+                    const listItem=  document.createElement('LI');
+                    listItem.textContent=  `${userInfo.id} - ${userInfo.name}`;
+        
+                    fragment.appendChild(listItem);
+                    
+                }
+        
+                
+                list.appendChild(fragment);
+
+
+            }).catch(err=>console.log(err))
+
+
+
+})
