ba9f0a8de5b94c0acdf4d74c7e19b85a13a30c9a
Andres Zapata <azapata@uc.edu.co>
2021-04-05 23:06:06 -0500

    API DRAG AND DROP

--- clase-22/scripts.js
+++ clase-22/scripts.js
@@ -1,5 +1,7 @@
 const pendingTasks = document.getElementById('pending-tasks')
 const finishedTasks = document.getElementById('finished-tasks')
+
+
 //dataTransfer
 //setData: Establece la información que queremos compartir
 //getData: Establece la información que queremos obtener
@@ -11,6 +13,7 @@ finishedTasks.addEventListener('dragstart', (e) => {
     e.dataTransfer.setData('text/plain', e.target.id)
 })
 
+// cuando se empieza arrastrar se agrega la clase active para resaltarla dentro de las demas en blanco
 pendingTasks.addEventListener('drag', (e) => {
     e.target.classList.add('active')
 })
@@ -19,6 +22,8 @@ finishedTasks.addEventListener('drag', (e) => {
     e.target.classList.add('active')
 })
 
+
+// cuando finalice el arrastre se remueve la clase active
 pendingTasks.addEventListener('dragend', (e) => {
     e.target.classList.remove('active')
 })
@@ -27,7 +32,13 @@ finishedTasks.addEventListener('dragend', (e) => {
     e.target.classList.remove('active')
 })
 
+
+
+
 //OBLIGATORIO, SI NO, NO FUNCIONA
+
+//configuracion de dropzone para zona finished
+
 finishedTasks.addEventListener('dragover', (e) => {
     e.preventDefault()
 })
@@ -36,10 +47,12 @@ finishedTasks.addEventListener('drop', (e) => {
     e.preventDefault()
     const element = document.getElementById(e.dataTransfer.getData('text'))
     element.classList.remove('active')
+    element.classList.add('finished')
     finishedTasks.appendChild(pendingTasks.removeChild(element))
 })
 
 
+//configuracion de dropzone para zona pending
 
 pendingTasks.addEventListener('dragover',(e)=>{
     e.preventDefault();
@@ -50,6 +63,7 @@ pendingTasks.addEventListener('drop',(e)=>{
 
     const element = document.getElementById(e.dataTransfer.getData('text'))
     element.classList.remove('active')
+    element.classList.remove('finished')
     pendingTasks.appendChild(finishedTasks.removeChild(element))
 
 })
